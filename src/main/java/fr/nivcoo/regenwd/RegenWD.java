d1e78e232a41200b7cd1acb9e3f89780f646838c
nivcoo <nico64800@live.fr>
2021-07-08 23:01:21 +0200

    first

---
+++ src/main/java/fr/nivcoo/regenwd/RegenWD.java
@@ -0,0 +1,25 @@
+package fr.nivcoo.regenwd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class RegenWD extends JavaPlugin {
+
+	private static RegenWD INSTANCE;
+
+	@Override
+	public void onEnable() {
+		INSTANCE = this;
+		getCommand("regenwd").setExecutor(new RegenWDCommands());
+		
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	public static RegenWD get() {
+		return INSTANCE;
+	}
+
+}
